306986a3d8070de363b4ac42d077ab9487854424
Jan-Moller <ja-moeller@web.de>
2025-11-18 22:33:07 +0100

    fetch translation name

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let currentPokemonArray = [];
 async function init() {
     includeHTML();
     await getPokemonOverviewData()
+    renderPokemonOverview()
 
 }
 
@@ -24,26 +25,50 @@ async function includeHTML() {
 async function getData(path = "") {
     let response = await fetch(BASE_URL + path)
     let responseToJson = await response.json()
-    return responseToJson; 
+    return responseToJson;
 
+}
+
+function getSpriteUrl(details) {
+    return (
+        details?.sprites?.other?.dream_world?.front_default ||
+        details?.sprites?.other?.["official-artwork"]?.front_default ||
+        details?.sprites?.front_default ||
+        ""
+    );
 }
 
 async function getPokemonOverviewData() {
     let pokemons = await getData("/pokemon?limit=10&offset=0")
     for (let i = 0; i < pokemons.results.length; i++) {
         const pokemon = pokemons.results[i];
-        let details = await getData(`/pokemon/${pokemon.name}`)
+        let details = await getData(`/pokemon/${pokemon.name}`);
+        let german_name = await getData(`/pokemon-species/${pokemon.name}`)
 
         currentPokemonArray.push({
             id: i + 1,
             name: pokemon.name,
             type: details.types.map(type => type.type.name),
-            img_url: details.sprites.other.dream_world.front_default,
+                img_url: getSpriteUrl(details),
             height: details.height,
-            weight: details.weight, 
+            weight: details.weight,
             base_experience: details.base_experience,
+            translated_name_de: german_name.names.find(n => n.language.name === "de")?.name || pokemon.name,
         })
     }
     console.log(currentPokemonArray);
-    
+}
+
+function renderPokemonOverview() {
+let pokemonRef = document.getElementById('pokemon_overview');
+
+for (let i = 0; i < currentPokemonArray.length; i++) {
+    const name = currentPokemonArray[i].translated_name_de;
+    pokemonRef.innerHTML += /*html*/ `
+    <article>
+        <h2>${name}</h2>
+        <img src=${currentPokemonArray[i].img_url} alt="Bild eines Pokemons">
+    </article>
+    `
+}
 }
